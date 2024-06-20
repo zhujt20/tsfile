44957290ff4915c3c3a505bc1cb77a590eab1e8c
Haonan <hhaonan@outlook.com>
2024-06-21 01:58:21 +0800

    Update __init__.py

--- python/tsfile/__init__.py
+++ python/tsfile/__init__.py
@@ -17,10 +17,10 @@
 #
 import platform
 import os
-from .tsfile import read_tsfile, write_tsfile
 
 if platform.system() == "Windows":
     extra_dll_dir = os.path.join(os.path.dirname(__file__), ".")
     os.add_dll_directory(extra_dll_dir)
     print(extra_dll_dir)
-  
+
+from .tsfile import read_tsfile, write_tsfile
